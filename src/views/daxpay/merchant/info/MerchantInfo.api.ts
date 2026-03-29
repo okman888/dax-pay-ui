abd8e0fd466b2538fec940c6f60875b7aae594b2
chenliang3 <chenliang3@linewell>
2026-03-29 16:25:43 +0800

    feat(merchant): 添加获取商户费率设置的接口
    新增 FeeSetting 类型定义和 getFeeSetting API 接口，用于获取商户费率信息

--- src/views/daxpay/merchant/info/MerchantInfo.api.ts
+++ src/views/daxpay/merchant/info/MerchantInfo.api.ts
@@ -2,11 +2,28 @@ import { defHttp } from '@/utils/http/axios'
 import { Result } from '#/axios'
 import { Merchant } from '@/views/daxpay/admin/merchant/info/Merchant.api'
 
+export interface FeeSetting {
+  mchId?: string
+  mchNo?: string
+  mchName?: string
+  feeRate?: number
+  status?: string
+}
+
 /**
  * 获取商户信息
  */
 export const get = () => {
   return defHttp.get<Result<Merchant>>({
     url: '/merchant/get',
+  })
+}
+
+/**
+ * 获取商户费率设置
+ */
+export const getFeeSetting = () => {
+  return defHttp.get<Result<FeeSetting>>({
+    url: '/merchant/getFeeSetting',
   })
 }
